496a8e6c39a44ac250094986b2fb654b6073a637
user <brewbaker@yandex.com>
2017-08-11 09:46:18 +0300

    Create head.php

---
+++ app/parts/head.php
@@ -0,0 +1,24 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!--<link rel="stylesheet" href="css/mockup.css">-->
+      <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+
+      <script src="https://use.fontawesome.com/fe69f16b44.js"></script>
+      <link rel="stylesheet" href="css/device.css">
+      <link rel="stylesheet" href="css/bulma.css">
+      <link rel="stylesheet" href="css/resp.css">
+      <style>iframe.window {
+    height: 600px;
+    width: 100%;
+}.iphone-6.silver:after {
+    background: 0;
+    box-shadow: none;
+}img.paymonlogo {
+    max-height: 100%;
+    width: 90px;
+}.dwn{    margin-top: 20px;
+    background-color: whitesmoke;
+    padding: 25px;}
+    </style>
+  </head>
